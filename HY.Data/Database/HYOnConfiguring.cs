b7ed8ce9321fa16a933c0992e91109fe3911464f
mikegulik@gmail.com <mikegulik@gmail.com>
2020-01-26 18:30:12 -0600

    Adding to home computer

---
+++ HY.Data/Database/HYOnConfiguring.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HY.Data.Database
+{
+    public static class HYOnConfiguring
+    {
+        public static void Configure(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.EnableDetailedErrors();
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=HellYeah;Trusted_Connection=True;ConnectRetryCount=0");
+        }
+    }
+}
---
+++ HY.Data/Entities/EFHelpers/HYManyToMany.cs
@@ -0,0 +1,13 @@
+﻿namespace HY.Data.Entities.EFHelpers
+{
+    public class HYManyToMany<T,D> 
+        where T:HYBase
+        where D:HYBase
+    {
+        public int ChildId { get; set; }
+        public T Child { get; set; }
+
+        public int ParentId { get; set; }
+        public D Parent { get; set; }
+    }
+}
